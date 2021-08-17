fbee944dde291be244e6e425fa0aa015e53b7e84
Nat Waterworth <32358976+ProgrammerWaterworth@users.noreply.github.com>
2021-08-17 13:38:54 +0100

    Import Text Mesh Pro

--- Mobile 3D Application/Assets/Scripts/ModelExpandableChild.cs
+++ Mobile 3D Application/Assets/Scripts/ModelExpandableChild.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ModelExpandableChild : MonoBehaviour
 {
     Vector3 collapsedPosition;
     [SerializeField] Vector3 expandedTransformation;
+
+    //[Header("UI")]
+    TextMeshProUGUI objectNameTextGUI;
 
     private void Start()
     {
@@ -15,5 +20,16 @@ public class ModelExpandableChild : MonoBehaviour
     public void UpdatePosition(float _expansionMagnitude)
     {
         transform.localPosition =  Vector3.Lerp(collapsedPosition, collapsedPosition + expandedTransformation, _expansionMagnitude);
+    }
+
+    private void Update()
+    {
+        UpdateObjectNamePosition();
+    }
+
+    void UpdateObjectNamePosition()
+    {
+        if(objectNameTextGUI!=null)
+            objectNameTextGUI.transform.LookAt(Camera.main.transform);
     }
 }
